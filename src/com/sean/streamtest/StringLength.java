38463b3713e107b2619800d35a1ad0fb1d142da0
孙超 <baihousc@qq.com>
2021-08-30 14:34:37 +0800

    创建流的方式

--- src/com/sean/streamtest/StringLength.java
+++ src/com/sean/streamtest/StringLength.java
@@ -7,11 +7,16 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * @author admin
+ */
 public class StringLength {
     public static void main(String[] args) throws IOException {
         String str = new String(Files.readAllBytes(Paths.get("src/com/sean/streamtest/StringLength.txt")),
                 StandardCharsets.UTF_8);
         List<String> list = Arrays.asList(str.split("\n"));
+        //  Stream<T> filter(Predicate<? super T> predicate) : 产生一个流，其中包含当前流满足P的所有元素
+        // long count()  产生当前流中元素的数量。是一个终止操作。
         long count = list.stream().filter(v -> v.length() > 5).count();
         System.err.println(count);
     }
